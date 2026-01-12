6d0dabd74bc4440d0d3870217376371190c7d62d
Ritik Rana <ritikrana8596@gmail.com>
2026-01-12 10:25:05 +0530

    resume updated

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -687,7 +687,7 @@ export default function Portfolio() {
                 ritikrana8596@gmail.com
               </Button>
               <a
-                href="https://drive.google.com/file/d/1xvyeKg_B4nbMg7nfAKa4onIiVJSoMK6a/view?usp=sharing"
+                href="https://drive.google.com/file/d/1xvyeKg_B4nbMg7nfAKa4onIiVJSoMK6a/view?usp=drive_link"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="inline-flex items-center justify-center px-4 py-2 border border-cyan-500 text-cyan-400 hover:bg-cyan-500/10 font-mono bg-transparent hover:scale-105 transition-transform text-sm rounded-lg"
